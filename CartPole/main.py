f699c4fe5aaf4c3819eeb051071b2f491013449c
Szymon Ciemala <szymciem8@gmail.com>
2022-03-16 14:45:14 +0100

    initial update

---
+++ CartPole/main.py
@@ -0,0 +1,120 @@
+import gym
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Configuration parameters for the whole setup
+seed = 42
+gamma = 0.99 # Discount factor for past rewards
+max_steps_per_episode = 10000
+env = gym.make('CartPole-v1')
+env.seed(seed)
+eps = np.finfo(np.float32).eps.item() # Smallest number such that 1.0 + eps != 1.0
+
+
+# Implement Actor Critic network
+num_inputs = 4
+num_actions = 2
+num_hidden = 128
+
+inputs = layers.Input(shape=(num_inputs,))
+common = layers.Dense(num_hidden, activation="relu")(inputs)
+action = layers.Dense(num_actions, activation="softmax")(common)
+critic = layers.Dense(1)(common)
+
+model = keras.Model(inputs=inputs, outputs=[action, critic])
+
+optimizer = keras.optimizers.Adam(learning_rate=0.01)
+hubber_losss = keras.losses.Huber()
+action_probs_history = []
+critic_value_history = []
+rewards_history = []
+running_reward = 0
+episode_count = 0
+
+while True:
+    state = env.reset()
+    episode_reward = 0
+    with tf.GradientTape() as tape:
+        for timestamp in range(1, max_steps_per_episode):
+
+            # env.render() # -> Show attemps in the pop up window
+
+            state = tf.convert_to_tensor(state)
+            state = tf.expand_dims(state, 0)
+
+            action_probs, critic_value = model(state)
+            critic_value_history.append(critic_value[0,0])
+            # print(critic_value)
+
+            # print(np.squeeze(action_probs))
+
+            action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+            action_probs_history.append(tf.math.log(action_probs[0, action]))
+
+            state, reward, done, _ = env.step(action)
+            rewards_history.append(reward)
+            episode_reward += reward
+
+            if done:
+                break
+
+
+        running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
+
+        returns = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + gamma * discounted_sum
+            returns.insert(0, discounted_sum)
+
+
+        # Normalize
+        returns = np.array(returns)
+        returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+        returns = returns.tolist()
+
+        # Calculating loss values to update our network
+        history = zip(action_probs_history, critic_value_history, returns)
+        actor_losses = []
+        critic_losses = []
+
+        for log_prob, value, ret in history:
+            diff = ret - value
+            actor_losses.append(-log_prob * diff)
+
+            critic_losses.append(
+                hubber_losss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+            )
+
+        loss_value = sum(actor_losses) + sum(critic_losses)
+        print(loss_value)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        action_probs_history.clear()
+        critic_value_history.clear()
+        rewards_history.clear()
+
+
+    # Log details
+    episode_count += 1
+    if episode_count % 10 == 0:
+        template = "running reward: {:.2f} at espisode {}"
+        print(template.format(running_reward, episode_count))
+
+    if running_reward > 150:
+        print("Solved at episode {}!".format(episode_count))
+        break
+
+
+
+
+
+
+# env.reset()
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+# env.close()
---
+++ Mountain Car Discrete/mountain_car.py
@@ -0,0 +1,128 @@
+# MountainCar-v0
+
+from os import statvfs_result
+import gym
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+seed = 43
+gamma = 0.99
+max_steps_per_episode = 20000
+
+env = gym.make('MountainCar-v0')
+env.seed(seed)
+
+
+# Neural Network
+num_inputs = 2
+num_actions = 3
+num_hidden = 128
+
+inputs = layers.Input(shape=(num_inputs,))
+common = layers.Dense(num_hidden, activation="relu")(inputs)
+common = layers.Dense(num_hidden, activation="relu")(common)
+action = layers.Dense(num_actions, activation="softmax")(common)
+critic = layers.Dense(1)(common)
+
+model = keras.Model(inputs=inputs, outputs=[action, critic])
+
+optimizer = keras.optimizers.Adam(learning_rate=0.01)
+hubber_loss = keras.losses.Huber()
+action_probs_history = []
+critic_value_history = []
+rewards_history = []
+running_reward = 0
+episode_count = 0
+
+
+while True:
+    state = env.reset()
+    episode_reward = 0
+    
+    with tf.GradientTape() as tape:
+        while True:
+            if episode_count % 100 == 0:
+                env.render()
+
+            state = tf.convert_to_tensor(state)
+            state = tf.expand_dims(state, 0)
+
+            action_probs, critic_value = model(state)
+            critic_value_history.append(critic_value[0,0])
+
+            action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+            action_probs_history.append(tf.math.log(action_probs[0, action]))
+
+            state, reward, done, _ = env.step(action)
+            
+            # print(action)
+            # print(state)
+
+            if reward != -1:
+                reward = 1000
+            else:
+                reward = 0
+
+            reward += (abs(state[1])) ** 3
+
+            if state[0] > - 1:
+                r = (state[0] + 1) ** 2
+                reward += r
+            # elif state[0] < -0.7:
+            #     reward += 1
+
+            rewards_history.append(reward)
+            episode_reward += reward
+
+            if done:
+                break
+
+        running_reward = 0.05 * episode_reward + (1-0.05) * running_reward
+
+        returns = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + gamma * discounted_sum
+            returns.insert(0, discounted_sum)
+
+        # Normalize
+        returns = np.array(returns)
+        returns = (returns - np.mean(returns)) / (np.std(returns))
+        returns = returns.tolist()
+
+        history = zip(action_probs_history, critic_value_history, returns)
+        actor_losses = []
+        critic_losses = []
+
+        for log_prob, value, ret in history:
+            diff = ret - value
+            actor_losses.append(log_prob * diff)
+
+            critic_losses.append(
+                hubber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+            )
+
+        loss_value = sum(actor_losses) + sum(critic_losses)
+        # print(loss_value)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        action_probs_history.clear()
+        critic_value_history.clear()
+        rewards_history.clear()
+
+    # Log details
+    episode_count += 1
+    if episode_count % 10 == 0:
+        template = "running reward: {:.2f} at espisode {}"
+        print(template.format(running_reward, episode_count))
+
+    # if running_reward > 150:
+    #     print("Solved at episode {}!".format(episode_count))
+    #     break
+
+
+        
